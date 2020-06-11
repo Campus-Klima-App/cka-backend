5fa1e570dc9442a876e182eff615346def3a650e
Robert Deppe <contact@robertdeppe.de>
2020-06-11 23:28:05 +0200

    feat: general error handling

--- app.js
+++ app.js
@@ -4,10 +4,24 @@ const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/devices');
 const queryRoutes = require('./api/routes/query');
+const { request, response } = require('express');
 
 app.use(morgan('dev'));
 
 app.use('/devices', productRoutes);
 app.use('/query', queryRoutes);
+
+app.use((request, respone, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, request, response, next) => {
+    response.status(err.status || 500);
+    response.json({
+        message: error.message
+    })
+});
 
 module.exports = app;
