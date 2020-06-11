aeccc54fad087e081a8a3eadc0706a1a0a552653
Robert Deppe <contact@robertdeppe.de>
2020-06-11 23:19:08 +0200

    chore: add morgan package

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/devices');
 const queryRoutes = require('./api/routes/query');
+
+app.use(morgan('dev'));
 
 app.use('/devices', productRoutes);
 app.use('/query', queryRoutes);
