2592fd7aae3ac92ac1a0bee6f4d3a3643e57b72e
Robert Deppe <contact@robertdeppe.de>
2020-06-30 01:03:09 +0200

    fix: correct names of parameters on request

--- api/models/Datapoint.js
+++ api/models/Datapoint.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const datapointSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  _device_id: String,
-  _raw: String,
-  _time: String,
-  _field1: String,
-  _field2: String,
+  device_id: String,
+  raw: String,
+  time: String,
+  field1: String,
+  field2: String,
 });
 
 module.exports = mongoose.model("Datapoint", datapointSchema);
